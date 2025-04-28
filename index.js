417b0ad31e9e0f7f2920eaa750a47cc8f2a94d53
Iqrawaseem8219 <iqrawaseem674@gmail.com>
2025-04-28 02:05:21 -0700

    Add files via upload

---
+++ index.js
@@ -0,0 +1,20 @@
+
+//let numberOfStudents = parseInt(prompt("Enter the number of students:"));
+
+let age1 = parseInt(prompt("Enter the age of student 1:"));
+let age2 = parseInt(prompt("Enter the age of student 2:"));
+let age3 = parseInt(prompt("Enter the age of student 3:"));
+let age4 = parseInt(prompt("Enter the age of student 4:"));
+let age5 = parseInt(prompt("Enter the age of student 5:"));
+let age6 = parseInt(prompt("Enter the age of student 6:"));
+let age7 = parseInt(prompt("Enter the age of student 7:"));
+let age8 = parseInt(prompt("Enter the age of student 8:"));
+let age9 = parseInt(prompt("Enter the age of student 9:"));
+let age10 = parseInt(prompt("Enter the age of student 10:"));
+let age11 = parseInt(prompt("Enter the age of student 11:"));
+let age12 = parseInt(prompt("Enter the age of student 12:"));
+
+let totalAge = age1 + age2 + age3;
+
+let averageAge = totalAge / numberOfStudents;
+alert("The average age is: " + averageAge);
